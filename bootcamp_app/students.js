0c0be87aa5e93635c5634af750019397f395ea3d
Russell Cape <russellcape98@gmail.com>
2019-11-14 19:28:19 +0000

    Parameterized Queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,27 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res.rows);
-// })
-// .catch(err => console.error('query error', err.stack));
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students JOIN cohorts 
+  ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students INNER JOIN cohorts
-ON cohorts.id = cohort_id
-where cohorts.name like '%${process.argv[2]}%' 
-LIMIT ${process.argv[3] || 5}
-`)
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+      })
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error(`query error`, err.stack));
